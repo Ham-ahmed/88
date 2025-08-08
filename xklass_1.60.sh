20151bd13454159d7ada9848212b1a388e731d1a
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-08-08 15:32:08 +0300

    Add files via upload

---
+++ xklass_1.60.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-xklass_1.60.20250806_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/88/refs/heads/main/enigma2-plugin-extensions-xklass_1.60.20250806_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-xklass_1.60.20250806_all.ipk
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "#########################################################"
+echo "#                INSTALLED SUCCESSFULLY                 #"
+echo "#                    ON - Panel v6                      #"
+echo "#             Enigma2 restart is required               #"
+echo "#        .::UPLOADED BY  >>>>   HAMDY_AHMED::.          #"
+echo "#     https://www.facebook.com/share/g/18qCRuHz26/      #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+wait
+killall -9 enigma2
+exit 0
